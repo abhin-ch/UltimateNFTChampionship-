9dfda7ebe5da5231f17644e5de1578a7d35b788d
Henry <44884792+hoomji@users.noreply.github.com>
2022-11-14 13:18:22 -0500

    Create moralis.ts

---
+++ src/utils/moralis.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+//get the lowest floor price of an nft contract from the Moralis API
+export const getLowestPrice = async (address: string) => {
+  const options = {
+    method: "GET",
+    url: `https://deep-index.moralis.io/api/v2/nft/${address}/lowestprice`,
+    params: { chain: "eth", marketplace: "opensea" },
+    headers: { accept: "application/json", "X-API-Key": "test" },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+      return response.data;
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+};
